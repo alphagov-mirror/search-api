fd459ddee70f11b6cba1e35581eb18adaad7b3eb
Michael Walker <mike@barrucadu.co.uk>
2020-01-13 14:16:01 +0000

    Use lazy enumerators in LTR training data generation
    The current training data generation uses a lot of memory, as it
    holids in memory all at the same time:
    
    - Bigquery results
    - Relevancy judgements
    - Augmented relevancy judgements
    - SVM data
    
    And there's other things (like SVM data in another form) which
    temporarily live in memory, before being garbage collected.  That's a
    lot to hold in memory at once.
    
    This commit changes the data generation to use lazy enumerators, so
    everything other than the bigquery data (which is still held in memory
    in full) is only computed when needed.
    
    Unfortunately, we can no longer print out a progress metric when
    computing the augmented relevancy judgements, because to get the total
    number of judgements would require forcing that iterator, which
    defeats the point.
    
    Finally, I've reduced the number of queries cached in the
    judgement-augmentor to 25, as we're getting quite a lot of data from
    elasticsearch at a time.

--- lib/learn_to_rank/data_pipeline/embed_features.rb
+++ lib/learn_to_rank/data_pipeline/embed_features.rb
@@ -1,33 +1,33 @@
 module LearnToRank::DataPipeline
   class EmbedFeatures
     # EmbedFeatures takes a set of relevancy judgements and add features to them.
-    # INPUT: [{ query: "a1", "id": "123456", score: 1 }]
-    # OUTPUT: [{ query: "a1", "id": "123456", score: 1,
-    #            view_count: 2, description_score: 2, title_score: 4 ... }]
+    # INPUT: enumerator of { query: "a1", "id": "123456", score: 1 }
+    # OUTPUT: lazy enumerator of { query: "a1", "id": "123456", score: 1, view_count: 2, description_score: 2, title_score: 4 ... }
     def initialize(judgements)
       @judgements = judgements
-      @cached_queries = {}
     end
 
     def augmented_judgements
-      count = Float(judgements.count)
-      res = judgements.compact.map.with_index do |judgement, i|
-        logger.info "#{i}/#{count}: #{(i / count) * 100}%"
-        feats = features(judgement)
+      cached_queries = {}
+      augmented = judgements.lazy.map do |judgement|
+        next nil unless judgement
+
+        feats = features(judgement, cached_queries)
         next nil unless feats
+
+        cached_queries = {} if cached_queries.keys.count > 25
 
         judgement.merge(feats)
       end
-      flush_cached_queries
-      res.compact
+      augmented.reject(&:nil?)
     end
 
   private
 
     attr_reader :judgements
 
-    def features(judgement)
-      doc = fetch_document(judgement)
+    def features(judgement, cached_queries)
+      doc = fetch_document(judgement, cached_queries)
       return unless doc
 
       feats = LearnToRank::Features.new(
@@ -48,7 +48,7 @@ module LearnToRank::DataPipeline
       { features: feats }
     end
 
-    def fetch_document(judgement)
+    def fetch_document(judgement, cached_queries)
       # TODO: Could we use MTerm Vectors API for this?
       begin
         retries ||= 0
@@ -58,11 +58,9 @@ module LearnToRank::DataPipeline
           "fields" => %w(popularity content_id title format description link public_timestamp organisation_content_ids updated_at indexable_content),
           "count" => %w[20],
         }
-        @cached_queries[judgement[:query]] ||= do_fetch(query)
-        results = @cached_queries[judgement[:query]]
-        if @cached_queries.keys.count > 50
-          flush_cached_queries
-        end
+        cached_queries[judgement[:query]] ||= do_fetch(query)
+        results = cached_queries[judgement[:query]]
+
         results.find { |doc| doc["link"] == judgement[:link] }
       rescue StandardError => e
         puts e
@@ -70,10 +68,6 @@ module LearnToRank::DataPipeline
         retry if (retries += 1) < 3
         nil
       end
-    end
-
-    def flush_cached_queries
-      @cached_queries = {}
     end
 
     def do_fetch(query)
